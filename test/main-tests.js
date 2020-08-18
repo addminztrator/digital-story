ae4449adca31a6983de7dc08a818a45648b03e57
A. Saad Imran <aimran96@protonmail.com>
2020-08-18 18:03:05 -0400

    - fix tests to work locally and in the pipeline

--- test/main-tests.js
+++ test/main-tests.js
@@ -1,9 +1,14 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
+// for local tests
+//var host = 'http://localhost:5000';
+
+var host = 'http://digital-story.herokuapp.com';
+
 // Try requesting the main page and make sure server is running
 it('App running', function(done) {
-    request('http://localhost:5000' , function(error, response, body) {
+    request(host , function(error, response, body) {
         expect(body).to.not.equal(null);
 		expect(body).to.not.equal(undefined);
         done();
@@ -12,15 +17,15 @@ it('App running', function(done) {
 
 describe('Random url addresses redirect to main page', function(){
 	describe('Perform get requests on random URLs', function(){
-		it('Get http://localhost:5000/something', function(done) {
-			request('http://localhost:5000/something' , function(error, response, body) {
+		it('Get ' + host +'/something', function(done) {
+			request(host + '/something' , function(error, response, body) {
 				expect('Location', '/');
 				done();
 			});
 		});
 		
-		it('Get http://localhost:5000/somethingelse/asds asd jnj454369?/ewrkmdf', function(done) {
-			request('http://localhost:5000/somethingelse/asds asd jnj454369?/ewrkmdf' , function(error, response, body) {
+		it('Get ' + host + '/somethingelse/asds asd jnj454369?/ewrkmdf', function(done) {
+			request(host + '/somethingelse/asds asd jnj454369?/ewrkmdf' , function(error, response, body) {
 				expect('Location', '/');
 				done();
 			});
@@ -29,15 +34,15 @@ describe('Random url addresses redirect to main page', function(){
 	
 	describe('Perform post requests on random URLs', function()
 	{
-		it('post http://localhost:5000/something', function(done) {
-			request.post({url:'http://localhost:5000/something', form: {key:'value'}} , function(error, response, body) {
+		it('post ' + host +'/something', function(done) {
+			request.post({url:host+'/something', form: {key:'value'}} , function(error, response, body) {
 				expect('Location', '/');
 				done();
 			});
 		});
 		
-		it('post http://localhost:5000/somethingelse/asds asd jnj454369?/ewrkmdf', function(done) {
-			request.post({url:'http://localhost:5000/somethingelse/asds asd jnj454369?/ewrkmdf', form: {key:'value'}} , function(error, response, body) {
+		it('post ' + host +'/somethingelse/asds asd jnj454369?/ewrkmdf', function(done) {
+			request.post({url:host+'/somethingelse/asds asd jnj454369?/ewrkmdf', form: {key:'value'}} , function(error, response, body) {
 				expect('Location', '/');
 				done();
 			});
