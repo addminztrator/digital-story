e3c4e1c9b716a2a552df129782fc2a04063d8747
A. Saad Imran <aimran96@protonmail.com>
2020-08-19 01:18:09 -0400

    - add logging

--- index.js
+++ index.js
@@ -18,6 +18,30 @@ var crypto = require("crypto")
 
 const { Pool } = require('pg');
 
+const Logger = require('logdna');
+const options = {
+    env: 'env'
+    , app: 'digital-story'
+    , hostname: 'digital-story'
+    , index_meta: true
+};
+
+var _log = console.log;
+var _error = console.error;
+
+var logger = Logger.setupDefaultLogger('c43a327f0bc60da87e86d341e7a11e3e', options);
+
+
+var log = function() {
+    logger.log([...arguments].join(' '));
+    _log.apply(console, arguments);
+};
+
+var error = function() {
+    logger.error([...arguments].join(' '));
+    _error.apply(console, arguments);
+};
+
 /*
 This statement is needed for testing on a local system. 
 Must be replaced with the statement below before deployment.
@@ -59,6 +83,7 @@ var env = nunjucks.configure(['views/'], { // set folders with templates
     express: app
 });
 app.get('/', async (req, res) => {
+	log('A user visited main page');
 	var loggedIn = false
 	var posts = null
 	try {
@@ -67,6 +92,7 @@ app.get('/', async (req, res) => {
 		client.release();
 	} catch (err) {
 	    console.error(err);
+		error('A critical database error occured while grabbing posts for the main page');
 	}
 	if (req.cookies.user){
 		var cookie = req.cookies.user.split(",")
@@ -74,6 +100,7 @@ app.get('/', async (req, res) => {
 		var hash = cookie[1]
 		if (hash == sha512(username)){
 			loggedIn = true
+			log('User was logged in while visiting the main page');
 			if (posts){
 				res.render('index.html', {signedIn: loggedIn, username: username, id: username, posts: posts.rows});
 			}
@@ -84,6 +111,7 @@ app.get('/', async (req, res) => {
 		}
 	}
 	if (!loggedIn){
+		log('User was NOT logged in while visiting the main page');
 		if (posts){
 			res.render('index.html', {posts: posts.rows}); 
 		}
@@ -94,12 +122,14 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/logout', function (req, res){
+	log('User logged out');
 	res.clearCookie('user');
 	res.redirect('/');
 })
 
 app.get('/delete/:id', async function (req, res){
 	if (req.cookies.user){
+		log('User attempting to delete post');
 		var cookie = req.cookies.user.split(",")
 		var username = cookie[0]
 		var hash = cookie[1]
@@ -109,12 +139,14 @@ app.get('/delete/:id', async function (req, res){
 			  const result = await client.query(`SELECT * FROM posts where id='${req.params.id}'`);
 			  if (result.rows[0].username == username){
 				  await client.query(`Delete FROM posts where id='${req.params.id}'`);
+				  log('User successfully deleted post');
 				  res.redirect("/");
 			  }
 			} catch(err){
-				
+				log('A critical database error occured while user was attempting to delete post');
 			}
 		}
+		error('User not authorized to delete post');
 	}
 })
 
